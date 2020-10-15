a9f4a1e2a95e570f99b91b834f705b8b0f6baa78
Monish <monishra.1994@outlook.com>
2020-10-15 22:55:34 +0530

    removed build from .gitignore

---
+++ build/precache-manifest.6530181090053b707dd5a105180abbdd.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8a3668d51d8cfdeafb77321130893a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3343c87ffee381cdd1e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9e4f6a6ab93234dd4166",
+    "url": "/static/css/main.bc68ca2b.chunk.css"
+  },
+  {
+    "revision": "b3343c87ffee381cdd1e",
+    "url": "/static/js/2.0b223030.chunk.js"
+  },
+  {
+    "revision": "eb94d6017d8f0c633f4acf9346a3ecc8",
+    "url": "/static/js/2.0b223030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4f6a6ab93234dd4166",
+    "url": "/static/js/main.b2148880.chunk.js"
+  },
+  {
+    "revision": "37bad5e3a0a47f9c5bbd",
+    "url": "/static/js/runtime-main.f154848c.js"
+  },
+  {
+    "revision": "0fbb9bd432d4a73fb3ffdca712adb6c0",
+    "url": "/static/media/about-img.0fbb9bd4.jpg"
+  },
+  {
+    "revision": "a17db9bff158ab3f9bbfc967c08b4361",
+    "url": "/static/media/all-title-bg.a17db9bf.jpg"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "d4ef14eaedcb3452a56f23392e2b4773",
+    "url": "/static/media/portfolio-img-01.d4ef14ea.jpg"
+  },
+  {
+    "revision": "97bcb6cf8d1abbdf575562d691de47ff",
+    "url": "/static/media/portfolio-img-02.97bcb6cf.jpg"
+  },
+  {
+    "revision": "31be4092a294eccbc26202555eebc928",
+    "url": "/static/media/portfolio-img-03.31be4092.jpg"
+  },
+  {
+    "revision": "b1e2820fd953ea471e128c26690fbeee",
+    "url": "/static/media/portfolio-img-04.b1e2820f.jpg"
+  },
+  {
+    "revision": "01f68e6b1714feba63502a544776a444",
+    "url": "/static/media/portfolio-img-05.01f68e6b.jpg"
+  },
+  {
+    "revision": "f9b23705464729c63de811a1a0de8b04",
+    "url": "/static/media/post-img-02.f9b23705.jpg"
+  },
+  {
+    "revision": "6ef6d6f5e88287be23c730515b4a1040",
+    "url": "/static/media/post-img-03.6ef6d6f5.jpg"
+  },
+  {
+    "revision": "b29bc2e52fb3a0912afe88d18d1163b1",
+    "url": "/static/media/services-img-01.b29bc2e5.jpg"
+  },
+  {
+    "revision": "2314a974301e372f60bcfbd0ec4b7d63",
+    "url": "/static/media/services-img-02.2314a974.jpg"
+  },
+  {
+    "revision": "cd40b8e1b603b06eb8b5142b35cbf988",
+    "url": "/static/media/services-img-03.cd40b8e1.jpg"
+  },
+  {
+    "revision": "9bb39b3d3f788f28f57ebd54a045e1c1",
+    "url": "/static/media/slider-01.9bb39b3d.jpg"
+  },
+  {
+    "revision": "73582b261d55f090c7b53e21deb75df3",
+    "url": "/static/media/slider-02.73582b26.jpg"
+  },
+  {
+    "revision": "f40271971dcbad0fc6e1684867093817",
+    "url": "/static/media/slider-03.f4027197.jpg"
+  }
+]);
